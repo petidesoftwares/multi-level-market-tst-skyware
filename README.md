180aee92fb0acbb6dc555762fdf4a4c4644c6149
petidesoftwares <petideenterprise@gmail.com>
2020-10-08 16:30:11 +0100

    ReadMe and updated

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # multi-level-market-tst-skyware
     #This is a test project to simulate member registration and database
     #for the first 3 generation (DOWN LINE) of a single member 
-    #in a networking firm
+    #in a networking firm.
+#Setup.
+    #For simulation purposes, run functions.php file first.
+    #The database credentials are default for xampp server.
--- php/functions.php
+++ php/functions.php
@@ -5,9 +5,9 @@
     $conn = Connect_DB::createConnection();
     // Connect_DB::dropDB($conn,Consts::DB);
     
-    // createDB($conn,Consts::DB);
-    // Connect_DB::selectDB($conn);
-    // createTables($conn);
-    // Connect_DB::closeConnection($conn);
+    createDB($conn,Consts::DB);
+    Connect_DB::selectDB($conn);
+    createTables($conn);
+    Connect_DB::closeConnection($conn);
 
 ?>
